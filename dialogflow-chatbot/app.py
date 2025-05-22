1dc5815efc4c595ba0554be484109c5860f002bd
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-05-22 08:55:34 +0500

    Added Fast API backendwith MySQL driver

--- dialogflow-chatbot/app.py
+++ dialogflow-chatbot/app.py
@@ -2,28 +2,38 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Any, Dict
+from db_helper import track_order_by_id
 
 app = FastAPI()
 
+@app.get("/")
+async def get():
+    return{
+        "status": "ok"
+    }
 
-@app.post("/")
+@app.post("/webhook")
 async def dialogflow_webhook(request: Request):
     payload = await request.json()
     
-    # i.e order.track
     intent        = payload['queryResult']['action']
     param         = payload['queryResult']['parameter']
-    outputContext = payload['queryResult']['outputContexts']['name']
  
-    # Example logic based on intent
     if intent == "order.track":
-        # we need to call track order here
-        track_order(parameter=param)
+        order_id   = param.get('order_id')
+        return await track_order(order_id)
+    
+    return JSONResponse(content={"fulfillmentText": "Intent not handled."})
         
         
-async def track_order(parameter:dict):
+async def track_order(order_id: str|int):
+    order = track_order_by_id(order_id)
+    if order:
+        message = f"Order {order['order_id']} is currently {order['status']}."
+    else:
+        message = "Sorry, we couldn't find your order."
+
     return JSONResponse(content={
-        "fulfillmentText": "Your order is being processed.",
-        "parameter": {parameter}
+        "fulfillmentText": message
     })
     
