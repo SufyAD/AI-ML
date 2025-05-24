50b4c9a229b319a7366d15ba49028bb0ae6edc12
Sufyan Ahmed <muhammadsufyan20@gmail.com>
2025-05-25 01:40:08 +0500

    Added order_add function successfully tested with dialogflow chatbot

--- dialogflow-chatbot/app.py
+++ dialogflow-chatbot/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from db_helper import get_order_status, get_next_order_id, get_total_order_price, insert_order_item
+import db_helper
+import api_helper
 
 app = FastAPI(
     title="Order Tracker API",
@@ -14,57 +15,66 @@ async def get():
         "status": "ok"
     }
 
+inprog_order = {}
+
 @app.post("/")
 async def handle_webflow(request: Request):
     try:
         payload = await request.json()
         
-        intent = payload['queryResult']['intent']['displayName']
-        param  = payload['queryResult']['parameters']
+        intent          = payload['queryResult']['intent']['displayName']
+        param           = payload['queryResult']['parameters']
         output_contexts = payload['queryResult']['outputContexts']
-        # session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
+        session_id      = api_helper.get_session_id(output_contexts[0]["name"]) # of existing order
 
         # routing table for intents and their respective functions
-        
         intent_handler_dict = { # TODO: Updates the context
-            'order.add - context: ongoing-order': order_add,
+            'order.add : context: place an order': order_add,
+            'order.add - context: ongoing-order' : order_add,
             'order.remove - context: ongoing-order': order_cancel,
             'order.complete - context: ongoing-order': order_complete,
             'order.track - context: Take User ID': order_track
          }
-        
-        return intent_handler_dict[intent](param)
-
-        
+        return intent_handler_dict[intent](param, session_id)
+    
     except Exception as e:
         print(f"Webhook error: {e}")
         return JSONResponse(status_code=500, content={"error": str(e)})
+
+
+def order_add(parameters: dict, session_id: str):
+    order_items = parameters["gym-item"]
+    quantities  = parameters["number"]
+    
+    if len(order_items) != len(quantities):
+        fulfillmentText = "Please specify quantity with ordered item(s)"
+    else:
+        new_order = dict(zip(order_items, quantities))
+        # check if it is new or existing order
+        if session_id in inprog_order:
+            current_order = inprog_order[session_id]
+            current_order.update(new_order)
+            inprog_order[session_id] = current_order # update the inprogress order
+        else:
+            inprog_order[session_id] = new_order
         
+        order_str = api_helper.get_str_from_dict(inprog_order[session_id]) # for safe side
+        print(order_str)
+        fulfillmentText = f"So you have ordered: \n{order_str} so far. Do you need anything else?"
         
+        return JSONResponse(content={
+        "fulfillmentText": fulfillmentText
+    })
+
+            
+def order_complete(parameters: dict):
+    pass
         
 def order_track(parameters: dict):
-    # grab id from parameter
-    
-    quantity  = int(parameters['number'])
-    gym_item = int(parameters['gym-item'])
-    
-    insert_order_item(gym_item, quantity, order_id)
-
-    return JSONResponse(content={
-        "fulfillmentText": fulfillment_text
-    })
-    
-async def order_add(parameters: dict):
-    
-    return {
-        "fulfillmentText": "Order added"
-    }
+    pass
     
 async def order_cancel(parameters: dict):
-     return {
-        "fulfillmentText": "Order Cancelled"
-    }
-     
+    pass
      
 
 
--- dialogflow-chatbot/db_helper.py
+++ dialogflow-chatbot/db_helper.py
@@ -17,33 +17,25 @@ cnx = mysql.connector.connect(
 def insert_order_item(food_item, quantity, order_id):
     try:
         cursor = cnx.cursor()
-
         # Calling the stored procedure
         cursor.callproc('insert_order_item', (food_item, quantity, order_id))
-
         # Committing the changes
         cnx.commit()
-
         # Closing the cursor
         cursor.close()
-
         print("Order item inserted successfully!")
-
         return 1
 
     except mysql.connector.Error as err:
         print(f"Error inserting order item: {err}")
-
         # Rollback changes if necessary
         cnx.rollback()
-
         return -1
 
     except Exception as e:
         print(f"An error occurred: {e}")
         # Rollback changes if necessary
         cnx.rollback()
-
         return -1
 
 # Function to insert a record into the order_tracking table
